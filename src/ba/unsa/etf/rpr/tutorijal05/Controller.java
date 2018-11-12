e0cffea30515f41f13829ff8e0030e9f3c554e6b
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-12 10:26:54 +0100

    Digiton i Controller - broj 7

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -98,4 +98,17 @@ public class Controller {
         }
     }
 
+    public void sedam(ActionEvent actionEvent) {
+        if (unos.get().equals("0") && operacija.equals("")) {
+            unos.set("7");
+        } else if (!operacija.equals("") && brisi) {
+            unos.set("7");
+            brisi = !brisi;
+        } else if (!operacija.equals("")) {
+            unos.set(unos.get() + "7");
+        } else {
+            unos.set(unos.get() + "7");
+        }
+    }
+
 }

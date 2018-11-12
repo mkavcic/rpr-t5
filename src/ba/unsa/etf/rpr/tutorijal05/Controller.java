e611365cc8d6c08e6ef5a8dc6981de42fca984f9
mkavcic <mkavcic1@etf.unsa.ba>
2018-11-12 10:05:13 +0100

    Controller - broj 1

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,36 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+
 public class Controller {
+    private SimpleStringProperty unos;
+    private String operacija = "";
+    private boolean brisi = false;
+
+    public Controller() {
+        unos = new SimpleStringProperty("0");
+    }
+
+    public SimpleStringProperty ulazProperty() {
+        return unos;
+    }
+
+    public String getUlaz() {
+        return unos.get();
+    }
+
+    public void jedan(ActionEvent actionEvent) {
+        if (unos.get().equals("0") && operacija.equals("")) {
+            unos.set("1");
+        } else if (!operacija.equals("") && brisi) {
+            unos.set("1");
+            brisi = !brisi;
+        } else if (!operacija.equals("")) {
+            unos.set(unos.get() + "1");
+        } else {
+            unos.set(unos.get() + "1");
+        }
+    }
 
 }
